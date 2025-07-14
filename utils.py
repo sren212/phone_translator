091fb328aec31efa8ea1c4513d64003b41bdc0bb
Suwen Ren <sren7326@gmail.com>
2025-07-14 09:06:58 -0400

    fix typo in twilio auth

--- utils.py
+++ utils.py
@@ -6,9 +6,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def download_audio(recording_url):
-    twilio_sid = os.getenv("TWILIO_ACCOUNT_SID")
-    twilio_token = os.getenv("TWILIO_AUTH_TOKEN")
-
+    twilio_sid = os.getenv("TWILIO_SID")
+    twilio_token = os.getenv("TWILIO_AUTH")
+    print(f"SID: {twilio_sid}, Token set: {bool(twilio_token)}")
     resp = requests.get(recording_url, auth=(twilio_sid, twilio_token))
     resp.raise_for_status()
     return resp.content
